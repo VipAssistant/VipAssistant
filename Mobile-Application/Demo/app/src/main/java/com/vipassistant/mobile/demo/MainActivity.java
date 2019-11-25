d96d19540a91f6117973700e8319af0466841b12
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2019-11-25 23:03:34 +0300

    Implemented a testable mobile app that includes a calculator

--- Mobile-Application/Demo/app/src/main/java/com/vipassistant/mobile/demo/MainActivity.java
+++ Mobile-Application/Demo/app/src/main/java/com/vipassistant/mobile/demo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.vipassistant.mobile.demo;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,42 +13,61 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+	private EditText num1, num2, operation;
+	private TextView result;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		/* Stuff that comes default in MainActivity when initializing an Android Project */
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_main);
+//		Toolbar toolbar = findViewById(R.id.toolbar);
+//		setSupportActionBar(toolbar);
+//
+//		FloatingActionButton fab = findViewById(R.id.fab);
+//		fab.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View view) {
+//				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//						.setAction("Action", null).show();
+//			}
+//		});
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toolbar toolbar = findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
+		this.num1 = findViewById(R.id.num1);
+		this.num2 = findViewById(R.id.num2);
+		this.operation = findViewById(R.id.operation);
+		this.result = findViewById(R.id.result);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
+	public void calculateOnClick(View v) {
+		Double a = Double.parseDouble(this.num1.getText().toString());
+		Double b = Double.parseDouble(this.num2.getText().toString());
+		String op = this.operation.getText().toString();
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+		this.num1.setText("");
+		this.num2.setText("");
+		this.operation.setText("");
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
+		switch (op) {
+			case "+":
+				this.result.setText(String.format("Result = %.2f", a + b));
+				break;
+			case "-":
+				this.result.setText(String.format("Result = %.2f", a - b));
+				break;
+			case "*":
+				this.result.setText(String.format("Result = %.2f", a * b));
+				break;
+			case "/":
+				this.result.setText(String.format("Result = %.2f", a / b));
+				break;
+			default:
+				this.result.setText("Please only enter one of the following ops: +, -, *, /");
+				break;
 		}
-
-		return super.onOptionsItemSelected(item);
 	}
+
 }
