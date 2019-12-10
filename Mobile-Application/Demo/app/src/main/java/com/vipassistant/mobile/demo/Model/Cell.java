849374b2e2dec501211188c6a5a7003e879b73f5
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2019-12-10 23:26:19 +0300

    Finished Model layer of MVVM-TicTacToe

---
+++ Mobile-Application/Demo/app/src/main/java/com/vipassistant/mobile/demo/Model/Cell.java
@@ -0,0 +1,16 @@
+package com.vipassistant.mobile.demo.Model;
+
+import com.vipassistant.mobile.demo.Utilities.StringUtility;
+
+public class Cell {
+
+    public Player player;
+
+    public Cell(Player player) {
+        this.player = player;
+    }
+
+    public boolean isEmpty() {
+        return player == null || StringUtility.isNullOrEmpty(player.value);
+    }
+}
---
+++ Mobile-Application/Demo/app/src/main/java/com/vipassistant/mobile/demo/Model/Player.java
@@ -0,0 +1,13 @@
+package com.vipassistant.mobile.demo.Model;
+
+
+public class Player {
+
+    public String name;
+    public String value;
+
+    public Player(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+}
