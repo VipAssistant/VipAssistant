9c1fa1afd2821a8887fda02cc8836ea703b908c5
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2019-12-19 00:16:12 +0300

    Initialized Demo Platform Integration

--- Mobile-Application/Demo/app/src/main/java/com/vipassistant/mobile/demo/MainActivity.java
+++ Mobile-Application/Demo/app/src/main/java/com/vipassistant/mobile/demo/MainActivity.java
@@ -1,73 +1,62 @@
 package com.vipassistant.mobile.demo;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.navigation.NavigationView;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-	private EditText num1, num2, operation;
-	private TextView result;
+    private AppBarConfiguration mAppBarConfiguration;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		/* Stuff that comes default in MainActivity when initializing an Android Project */
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_main);
-//		Toolbar toolbar = findViewById(R.id.toolbar);
-//		setSupportActionBar(toolbar);
-//
-//		FloatingActionButton fab = findViewById(R.id.fab);
-//		fab.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View view) {
-//				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//						.setAction("Action", null).show();
-//			}
-//		});
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		this.num1 = findViewById(R.id.num1);
-		this.num2 = findViewById(R.id.num2);
-		this.operation = findViewById(R.id.operation);
-		this.result = findViewById(R.id.result);
-	}
-
-	public void calculateOnClick(View v) {
-		Double a = Double.parseDouble(this.num1.getText().toString());
-		Double b = Double.parseDouble(this.num2.getText().toString());
-		String op = this.operation.getText().toString();
-
-		this.num1.setText("");
-		this.num2.setText("");
-		this.operation.setText("");
-
-		switch (op) {
-			case "+":
-				this.result.setText(String.format("Result = %.2f", a + b));
-				break;
-			case "-":
-				this.result.setText(String.format("Result = %.2f", a - b));
-				break;
-			case "*":
-				this.result.setText(String.format("Result = %.2f", a * b));
-				break;
-			case "/":
-				this.result.setText(String.format("Result = %.2f", a / b));
-				break;
-			default:
-				this.result.setText("Please only enter one of the following ops: +, -, *, /");
-				break;
-		}
-	}
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
