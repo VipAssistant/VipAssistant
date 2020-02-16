5c3aee5ce84cc8b1cf6f0f86f797e5900b97e565
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2020-02-16 15:06:28 +0300

    Initialized new Fragment for Indoor Map

---
+++ Mobile-Application/Demo/app/src/main/java/com/vipassistant/mobile/demo/ui/MapNavigation/MapNavigationViewModel.java
@@ -0,0 +1,19 @@
+package com.vipassistant.mobile.demo.ui.MapNavigation;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MapNavigationViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public MapNavigationViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Map Navigation fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
